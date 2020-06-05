6e2c943d1536d525b0b9f3536a496c6da10431dd
Nabeel529886 <48175387+Nabeel529886@users.noreply.github.com>
2020-06-05 22:02:46 +0500

    added a button to remove todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import "./App.css"
 
 let id = 0
 
 const Todo = props => (
   <li>
     <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle} />
+    <button onClick={props.onDelete}>Delete</button>
     <span>{props.todo.text}</span>
   </li>
 )
@@ -21,6 +23,10 @@ export default class App extends React.Component{
     })
   }
 
+  removeTodo(id){
+    this.setState({todos: this.state.todos.filter(todo => todo.id !== id)})
+  }
+
   toggleTodo(id){
     this.setState({
       todos: this.state.todos.map(todo => {
@@ -35,12 +41,19 @@ export default class App extends React.Component{
   }
   render(){
     return (
-      <div>
+      <div id="main-div">
         <h1>BootCamp Todo List</h1>
+        <div>
+          <p>Total Todos: {this.state.todos.length}</p>
+          <p>Unchecked Todos: {this.state.todos.filter(todo => !todo.checked).length}</p>
+        </div>
         <button onClick={() => this.addTodo()}>Add A new Todo</button>
         <ul>
           {this.state.todos.map(todo => (
-            <Todo onToggle={() => this.toggleTodo(todo.id)} todo={todo}  />
+            <Todo 
+            onToggle={() => this.toggleTodo(todo.id)} 
+            onDelete={() => this.removeTodo(todo.id)}
+            todo={todo}  />
           ))}
         </ul>
       </div>
